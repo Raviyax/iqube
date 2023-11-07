3e72062df0142253194fbd924ce623565704e95e
Ravishan Jayathilake <2021is043@stu.ucsc.cmb.ac.lk>
2023-11-08 01:47:14 +0530

    modified:   app/config/config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,14 +1,14 @@
 <?php
 // DB Params 
-if($_SERVER['SERVER_NAME'] == 'localhost'){
+
     define('DB_HOST','localhost');
-    define('DB_USER','root');
-    define('DB_PASS','');
+    define('DB_USER','phpmyadmin');
+    define('DB_PASS','Priuscaa4025');
     define('DB_NAME','iqube');
     define('DBDRIVER','mysql');
-}else{
 
-}
+
+
 
 // app root - use something in app root
 define('APPROOT',dirname(dirname(__FILE__)));
