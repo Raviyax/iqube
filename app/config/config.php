6697271d7a6de87cc828d110ba2d617b9fc436ad
Ravishan Jayathilake <2021is043@stu.ucsc.cmb.ac.lk>
2023-11-16 00:28:27 +0530

    modified:   app/config/config.php

--- app/config/config.php
+++ app/config/config.php
@@ -2,7 +2,7 @@
 // DB Params 
 
     define('DB_HOST','localhost');
-    define('DB_USER','phpmyadmin');
+    define('DB_USER','root');
     define('DB_PASS','Priuscaa4025');
     define('DB_NAME','iqube');
     define('DBDRIVER','mysql');
@@ -13,7 +13,7 @@
 // app root - use something in app root
 define('APPROOT',dirname(dirname(__FILE__)));
 // URL root - use something in public root
-define('URLROOT','http://raviyax.me');
+define('URLROOT','https://iqube.raviyax.me');
 // Site name
 define('SITENAME','IQube');
 
