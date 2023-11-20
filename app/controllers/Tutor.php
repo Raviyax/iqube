51c1a5dc7525c0be2dec32d0f17f1270aeabf7df
Ravishan Jayathilake <2021is043@stu.ucsc.cmb.ac.lk>
2023-11-20 15:23:08 +0530

    deleted:    app/controllers/Subject_admin.php 	modified:   app/controllers/Tutor.php 	deleted:    app/models/Crud.php 	deleted:    app/views/Admin/users.php 	deleted:    app/views/Student/courses.php 	deleted:    app/views/Student/home.php 	deleted:    app/views/Student/messages.php 	deleted:    app/views/Student/profile.php 	deleted:    app/views/Student/schedule.php 	deleted:    app/views/Student/singleCourse.php 	deleted:    app/views/Student/syllabus.php 	deleted:    app/views/Student/tutors.php 	deleted:    app/views/Student/wishlist.php 	deleted:    app/views/Subject_admin/dashboard.php 	deleted:    app/views/Subject_admin/tutors.php 	deleted:    public/assets/css/Admin/users.css 	deleted:    public/assets/css/Student/courses.css 	deleted:    public/assets/css/Student/profile.css 	deleted:    public/assets/css/Student/schedule.css 	deleted:    public/assets/css/Student/singleCourse.css 	deleted:    public/assets/css/Student/studentStyle.css 	deleted:    public/assets/css/Student/syllabus.css 	deleted:    public/assets/css/Student/tutors.css 	deleted:    public/assets/css/Student/wishlist.css 	deleted:    public/assets/css/Subject_admin/dashboard.css 	deleted:    public/assets/css/Subject_admin/tutors.css 	deleted:    public/assets/img/Student/physics.png 	deleted:    public/assets/js/Student/dashboard.js 	deleted:    public/assets/js/Student/main.js

--- app/controllers/Tutor.php
+++ app/controllers/Tutor.php
@@ -1,7 +1,6 @@
 <?php
 class Tutor extends Controller {
 
-    public $Crud;
     public function index(){
         
         $data = [
@@ -26,31 +25,10 @@ class Tutor extends Controller {
 
     }
     public function lessons(){
-
-        $this->Crud = $this->model('Crud');
-        $result = $this->Crud->readData('courses');
-
-        if($_SERVER['REQUEST_METHOD'] == 'POST'){
-            if(isset($_POST['delete'])){
-                $condition ='id = :id';
-
-                $conditionparams = [
-                    'id' => $_POST['delete'],
-                ];
-                $this->Crud->deleteData('courses',$condition,$conditionparams);
-               
-                header('location:' . URLROOT . '/tutor/lessons');
-            }
-        }
-
-        
-        
-        
         
         $data = [
             'title' => 'Tutor',
-            'view' => 'lessons',
-            'result' => $result
+            'view' => 'lessons'
         ];
         $this->view('tutor/Lessons', $data);
        
@@ -61,28 +39,13 @@ class Tutor extends Controller {
 
         
 
-        $this->Crud = $this->model('Crud');
+
         
         $data = [
             'title' => 'Tutor',
             'view' => 'Upload New'
         ];
-        $this->view('tutor/upload', $data); 
-
-        if($_SERVER['REQUEST_METHOD'] == 'POST'){
-            $data = [
-                'chapter' => $_POST['chapter'],
-                'type' => $_POST['type'],
-                'price' => $_POST['price'],
-                
-                
-            ];
-            
-            $this->Crud->insertData('courses', $data);
-            header('location:' . URLROOT . '/tutor/l
-            essons');
-           
-           }
+        $this->view('tutor/upload', $data);
        
 
 
--- public/assets/js/Student/main.js
+++
@@ -1,7 +0,0 @@
-// Get the user icon element
-const userIcon = document.getElementById('usericon');
-
-// Add a click event listener to the user icon
-userIcon.addEventListener('click', function() {
-    window.location.href = 'Student/profile'; 
-});
