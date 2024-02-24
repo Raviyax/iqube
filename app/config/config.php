10407eedc95e5c2316fabed940d872ebbcd2533b
Ravishan Jayathilake <jayathilakeravishan@gmail.com>
2024-02-24 18:46:27 +0530

    modified:   app/config/config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,21 +1,21 @@
 <?php
 // DB Params
-if($_SERVER['SERVER_NAME'] == 'localhost'){
-    define('DB_HOST','localhost');
-    define('DB_USER','root');
-    define('DB_PASS','');
-    define('DB_NAME','iqube');
-    define('DBDRIVER','mysql');
-    define('URLROOT','http://localhost/iqube');
-}
-if($_SERVER['SERVER_NAME'] == 'https://groupproject.duckdns.org'){
+// if($_SERVER['SERVER_NAME'] == 'localhost'){
+//     define('DB_HOST','localhost');
+//     define('DB_USER','root');
+//     define('DB_PASS','');
+//     define('DB_NAME','iqube');
+//     define('DBDRIVER','mysql');
+//     define('URLROOT','http://localhost/iqube');
+// }
+
     define('DB_HOST','localhost');
     define('DB_USER','root');
     define('DB_PASS','Priuscaa4025');
     define('DB_NAME','iqube');
     define('DBDRIVER','mysql');
     define('URLROOT','https://groupproject.duckdns.org');
-}
+
 // app root - use something in app root
 define('APPROOT',dirname(dirname(__FILE__)));
 // URL root - use something in public root
