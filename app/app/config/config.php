3283178401e9c023366a6f6171bb78f304a189e2
Madasha Liyanage <2021is055@stu.ucsc.cmb.ac.lk>
2024-04-24 18:31:16 +0530

    modified:   app/app/config/config.php 	modified:   app/app/lib/Database.php

--- app/app/config/config.php
+++ app/app/config/config.php
@@ -1,19 +1,30 @@
 <?php
-if($_SERVER['SERVER_NAME'] == 'localhost'){
-    define('DB_HOST','localhost');
-    define('DB_USER','root');
-    define('DB_PASS','');
-    define('DB_NAME','iqube');
-    define('DBDRIVER','mysql');
-    define('URLROOT','http://localhost/iqube');
-}
+// if($_SERVER['SERVER_NAME'] == 'localhost'){
+//     define('DB_HOST','localhost');
+//     define('DB_USER','root');
+//     define('DB_PASS','');
+//     define('DB_NAME','iqube');
+//     define('DBDRIVER','mysql');
+//     define('URLROOT','http://localhost/iqube');
+// }
     // define('DB_HOST','db:3306');
     // define('DB_USER','root');
     // define('DB_PASS','Priuscaa4025');
     // define('DB_NAME','iqube');
     // define('DBDRIVER','mysql');
     // define('URLROOT','https://iqube.me');
-// app root - use something in app root
+
+
+define('DB_HOST','168.138.182.121');
+define('DB_USER','root');
+define('DB_PORT','10984');
+define('DB_PASS','xvhblk123');
+define('DB_NAME','iqube');
+define('DBDRIVER','mysql');
+define('URLROOT','http://localhost/iqube');
+
+
+
 define('APPROOT',dirname(dirname(__FILE__)));
 // URL root - use something in public root
 // Site name
--- app/app/lib/Database.php
+++ app/app/lib/Database.php
@@ -1,12 +1,28 @@
 <?php
 class Database
 {
+
+  // private function connect()
+  // {
+  //   $str = DBDRIVER . ":host=" . DB_HOST . ";dbname=" . DB_NAME;
+  //   $conn = new PDO($str, DB_USER, DB_PASS);
+  //   return $conn;
+  // }
+
   private function connect()
-  {
-    $str = DBDRIVER . ":host=" . DB_HOST . ";dbname=" . DB_NAME;
-    $conn = new PDO($str, DB_USER, DB_PASS);
-    return $conn;
-  }
+{
+    $dsn = DBDRIVER . ":host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME;
+    try {
+        $conn = new PDO($dsn, DB_USER, DB_PASS);
+        // Set PDO to throw exceptions on error
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    } catch (PDOException $e) {
+        // Handle connection errors
+        die("Connection failed: " . $e->getMessage());
+    }
+}
+
   public function query($query, $data = [], $type = 'object')
   {
     $conn = $this->connect();
