033599aebe4a900774a4ebf9d67d6f92718bc827
raviyax <2021is043@stu.ucsc.cmb.ac.lk>
2024-08-17 23:29:04 +0530

    deployement

---
+++ app/config/config.php
@@ -0,0 +1,27 @@
+<?php
+define('APPROOT', dirname(dirname(__FILE__)));
+// if($_SERVER['SERVER_NAME'] == 'localhost'){
+//     define('PATH_MYSQLDUMP', 'C:\xampp\mysql\bin\mysqldump.exe');
+//     define('BACKUP_DIRECTORY', APPROOT . '/site_backup');
+//     define('DB_HOST','localhost');
+//     define('DB_USER','root');
+//     define('DB_PASS','');
+//     define('DB_NAME','iqube');
+//     define('DBDRIVER','mysql');
+//     define('URLROOT','http://localhost/iqube');
+// }
+define('PATH_MYSQLDUMP', 'C:\xampp\mysql\bin\mysqldump.exe');
+define('BACKUP_DIRECTORY', APPROOT . '/site_backup');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'raviyax');
+define('DB_PASS', 'Priuscaa4025');
+define('DB_NAME', 'iqube');
+define('DBDRIVER', 'mysql');
+define('URLROOT', 'https://www.iqube.me');
+
+
+
+
+// URL root - use something in public root
+// Site name
+define('SITENAME', 'IQube');
